5735fea131f7f9d2747e89ab024a76cf56250f9e
Vicken Mouradian <vicken78@gmail.com>
2024-09-15 00:52:03 -0400

    Rewrite of ProducctModel. ProductViewModel and View next.

--- VMSales/Models/ProductModel.cs
+++ VMSales/Models/ProductModel.cs
@@ -2,119 +2,104 @@
 using System.Collections.Generic;
 using VMSales.ViewModels;
 using Dapper.Contrib.Extensions;
-using System.Collections.ObjectModel;
-using Caliburn.Micro;
 using System.ComponentModel;
+using System.Windows.Media;
 
 namespace VMSales.Models
 {
     [Table("product")]
     public partial class ProductModel : BaseViewModel
     {
-
-        private int _product_category_pk { get; set; }
-        private int _product_pk { get; set; }
-        private string _condition { get; set; }
-        private string _brand_name { get; set; }
-        private string _product_name { get; set; }
-        private string _description { get; set; }
-        private int _quantity { get; set; }
-        private decimal _cost { get; set; }
-        private string _sku { get; set; }
-        private decimal _listed_price { get; set; }
-        private int _instock { get; set; }
-        private string _listing_url { get; set; }
-        private string _listing_number { get; set; }
-        private DateTime _listing_date { get; set; }
-
-        private List<string> _category_list { get; set; }
-
-        private string _selected_category { get; set; }
-        public int category_pk { get; set; }
+        private int _product_pk;  
         [ExplicitKey]
-        [DefaultValue(0)]
         public int product_pk
         {
-            get { return _product_pk; }
+            get => _product_pk; 
             set
             {
-                if (_product_pk == value) return;
+                if (_product_pk != value)
                 _product_pk = value;
-                //RaisePropertyChanged("product_pk");
+                NotifyOfPropertyChange(() => product_pk);
             }
-        }      
+        }
+        private string _product_name;
         public string product_name
         {
-            get { return _product_name; }
+            get => _product_name; 
             set
             {
-                if (_product_name == value) return;
+                if (_product_name != value) 
                 _product_name = value;
-                //RaisePropertyChanged("product_name");
+                NotifyOfPropertyChange(() => product_name);
             }
         }
+        private string _description;
         public string description
         {
-            get { return _description; }
+            get => _description; 
             set
             {
-                if (_description == value) return;
+                if (_description != value) 
                 _description = value;
-                //RaisePropertyChanged("description");
+                NotifyOfPropertyChange(() => description);
             }
 
         }
+        private int _quantity;
         public int quantity
         {
-            get { return _quantity; }
-            set
+            get => _quantity; 
+            set 
             {
-                if (_quantity == value) return;
+                if (_quantity != value) 
                 _quantity = value;
-                //RaisePropertyChanged("quantity");
+                NotifyOfPropertyChange(() => quantity);
             }
-
         }
+        private decimal _cost;
         public decimal cost
         {
-            get { return _cost; }
+            get => _cost; 
             set
             {
-                if (_cost == value) return;
+                if (_cost != value) 
                 _cost = value;
-                //RaisePropertyChanged("cost");
+                NotifyOfPropertyChange(() => cost);
             }
 
         }
+        private string _sku;
         public string sku
         {
-            get { return _sku; }
+            get => _sku; 
             set
             {
-                if (_sku == value) return;
+                if (_sku != value)
                 _sku = value;
-                //RaisePropertyChanged("sku");
+                NotifyOfPropertyChange(() => sku);
             }
         }
+        private decimal _listed_price;
         public decimal listed_price
         {
-            get { return _listed_price; }
+            get => _listed_price;
             set
             {
-                if (_listed_price == value) return;
+                if (_listed_price != value) 
                 _listed_price = value;
-                //RaisePropertyChanged("listed_price");
+                NotifyOfPropertyChange(() => listed_price);
             }
 
         }
+        private int _instock;
         public int instock
-        {
-            get { return _instock; }
+        { 
+            get => _instock;  
             set
             {
-                if (_instock == value) return;
+                if (_instock != value)
                 _instock = value;
-                //RaisePropertyChanged("instock");
+                NotifyOfPropertyChange(() => instock);
             }
 
         }
@@ -122,127 +107,129 @@ namespace VMSales.Models
         private List<string> _conditionlist;
         public List<string> conditionlist
         {
-            get { return _conditionlist = new List<string> { "New","Used" }; }
+            get => _conditionlist = new List<string> { "New","Used" }; 
             set
             {
-                if (_conditionlist == value) return;
+                if (_conditionlist != value)
                 _conditionlist = value;
-                //RaisePropertyChanged("conditionlist");
+                NotifyOfPropertyChange(() => conditionlist);
             }
-
         }
-
+        
+        private string _condition;
         public string condition
         {
-            get { return _condition; }
+            get => _condition; 
             set
             {
-                if (_condition == value) return;
+                if (_condition != value)
                 _condition = value;
-                //RaisePropertyChanged("condition");
+                NotifyOfPropertyChange(() => condition);
             }
         }
-
+        private string _listing_url;
         public string listing_url
         {
-            get { return _listing_url; }
+            get => _listing_url; 
             set
             {
-                if (_listing_url == value) return;
+                if (_listing_url != value) 
                 _listing_url = value;
-                //RaisePropertyChanged("listing_url");
+                NotifyOfPropertyChange(() => listing_url);
             }
 
         }
+        private string _listing_number;
         public string listing_number
         {
-            get { return _listing_number; }
+            get => _listing_number; 
             set
             {
-                if (_listing_number == value) return;
+                if (_listing_number != value)
                 _listing_number = value;
-                //RaisePropertyChanged("listing_number");
+                NotifyOfPropertyChange(() => listing_number);
             }
 
 
         }
+        private DateTime _listing_date;
         public DateTime listing_date
         {
-            get { return _listing_date; }
+            get => _listing_date; 
             set
             {
-                if (_listing_date == value) return;
+                if (_listing_date == value)
                 _listing_date = value;
-                //RaisePropertyChanged("listing_date");
+                NotifyOfPropertyChange(() => listing_date);
             }
 
         }
-
+        private string _brand_name;
         public string brand_name
         {
-            get { return _brand_name; }
+            get => _brand_name;
             set
             {
-                if (_brand_name == value) return;
+                if (_brand_name != value)
                 _brand_name = value;
-                //RaisePropertyChanged("brand_name");
+                NotifyOfPropertyChange(() => brand_name);
             }
-
-
         }
 
         private bool _IsSelected;
         public bool IsSelected
         {
-            get { return _IsSelected; }
+            get => _IsSelected; 
             set
             {
                 _IsSelected = value;
-                //RaisePropertyChanged("IsSelected");
+                NotifyOfPropertyChange(() => IsSelected);
             }
         }
+        private int _product_category_pk;
         [ExplicitKey]
         public int product_category_pk
         { 
-        get { return _product_category_pk; }
+        get => _product_category_pk; 
         set {
-                if (_product_category_pk == value) return;
+                if (_product_category_pk != value)
                 _product_category_pk = value;
-                //RaisePropertyChanged("product_category_pk");
+                NotifyOfPropertyChange(() => product_category_pk);
             }
         }
         
         private int _category_fk { get; set; }
         public int category_fk
         {
-            get { return _category_fk; }
-            set {
-                if (_category_fk == value) return;
+        get => _category_fk; 
+        set {
+                if (_category_fk != value) 
                 _category_fk = value;
-                //RaisePropertyChanged("category_fk");
-                }
-        }
-
-        private string _category_name { get; set; }
-        public string category_name
-        {
-            get { return _category_name; }
-            set
-            {
-                if (_category_name == value) return;
-                _category_name = value;
-                //RaisePropertyChanged("category_name");
+                NotifyOfPropertyChange(() => category_fk);
             }
         }
 
-        private int _purchase_order_detail_fk { get; set; }
-        public int purchase_order_detail_fk
+        private string _category_name;
+        public string category_name
         {
-            get { return _purchase_order_detail_fk; }
+            get => _category_name; 
             set
             {
+                if (_category_name != value)
+                _category_name = value;
+                NotifyOfPropertyChange(() => category_name);
+            }
+        }
+
+        private int _purchase_order_detail_fk;
+        public int purchase_order_detail_fk
+        {
+            get => _purchase_order_detail_fk; 
+            set
+            {
+                if (_purchase_order_detail_fk != value)
                 _purchase_order_detail_fk = value;
-                //RaisePropertyChanged("purchase_order_detail_fk");
+                NotifyOfPropertyChange(() => purchase_order_detail_fk);
             }
         }
     }
